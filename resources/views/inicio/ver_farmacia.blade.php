1ce1f7759d8c7975ab5129c858a98570d50334d3
Sergio <scambaradorbigny@gmail.com>
2024-05-13 19:13:27 -0400

    resolviendo el css

--- resources/views/inicio/ver_farmacia.blade.php
+++ resources/views/inicio/ver_farmacia.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    {{-- <base href="/public"> --}}
-    {{-- @include('inicio.antesCabecera') --}}
+    <base href="/public">
+    @include('inicio.antesCabecera')
     <link rel="icon" type="image/x-icon" href="Farmacia/assets/favicon.ico" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <link href="Farmacia/css/styles.css" rel="stylesheet" />
